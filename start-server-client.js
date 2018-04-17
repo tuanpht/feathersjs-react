74b0673997cd47c5d42a931c590f61436bd0337f
Pham Tuan <pham.thanh.tuan@framgia.com>
2018-04-17 15:55:49 +0700

    Add docker mongodb

--- start-server-client.js
+++ start-server-client.js
@@ -1,2 +1,3 @@
 #!/bin/bash
-concurrently "(cd server && nodemon npm start)" "(cd client && npm start)"
+sudo docker-compose up -d && \
+    concurrently "(cd server && nodemon npm start)" "(cd client && npm start)"
